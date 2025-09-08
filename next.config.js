6cefba3c1c45c6298fb1f438a0c4a542c62450c3
generalsmart01 <michaelbrainy19@gmail.com>
2025-09-08 20:19:39 +0100

    refactor: Comment Out Unused Webpack Configuration in Next.js
    - Commented out the webpack configuration related to TerserPlugin for dropping console and debugger statements in production builds.
    - Streamlined the Next.js configuration file for improved clarity and maintainability.

--- next.config.js
+++ next.config.js
@@ -6,24 +6,24 @@ const nextConfig = {
     remotePatterns: [],
     domains: [],
   },
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      config.optimization.minimizer = config.optimization.minimizer || [];
+  // webpack: (config, { dev, isServer }) => {
+  //   if (!dev && !isServer) {
+  //     config.optimization.minimizer = config.optimization.minimizer || [];
       
-      const TerserPlugin = require('terser-webpack-plugin');
-      config.optimization.minimizer.push(
-        new TerserPlugin({
-          terserOptions: {
-            compress: {
-              drop_console: true,
-              drop_debugger: true,
-            },
-          },
-        })
-      );
-    }
-    return config;
-  },
+  //     const TerserPlugin = require('terser-webpack-plugin');
+  //     config.optimization.minimizer.push(
+  //       new TerserPlugin({
+  //         terserOptions: {
+  //           compress: {
+  //             drop_console: true,
+  //             drop_debugger: true,
+  //           },
+  //         },
+  //       })
+  //     );
+  //   }
+  //   return config;
+  // },
 };
 
 module.exports = nextConfig;
