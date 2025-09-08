96593d83207fcb2cde324ded614604d6e281f26c
generalsmart01 <michaelbrainy19@gmail.com>
2025-09-08 19:27:01 +0100

    refactor: Streamline Next.js Configuration and Console Statement Removal
    - Cleaned up the Next.js configuration by removing commented-out code related to console statement removal in production builds.
    - Ensured that the TerserPlugin configuration for dropping console and debugger statements remains intact for optimized production output.
    - Maintained the unoptimized image setting for clarity in the configuration.

--- next.config.js
+++ next.config.js
@@ -2,24 +2,21 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: true, // Disable Next.js image optimization
+    unoptimized: true,
     remotePatterns: [],
     domains: [],
   },
-  // Disable console statements in production
   webpack: (config, { dev, isServer }) => {
     if (!dev && !isServer) {
-      // Remove console statements in production builds
       config.optimization.minimizer = config.optimization.minimizer || [];
       
-      // Add TerserPlugin to remove console statements
       const TerserPlugin = require('terser-webpack-plugin');
       config.optimization.minimizer.push(
         new TerserPlugin({
           terserOptions: {
             compress: {
-              drop_console: true, // Remove console statements
-              drop_debugger: true, // Remove debugger statements
+              drop_console: true,
+              drop_debugger: true,
             },
           },
         })
